64fb9b4e9c11751f40dd48e78c61b511dd2d3120
Simen Lybekk <simen@lybekk.no>
2013-03-05 23:56:07 +0100

    .sh: Updates to minecraft.sh
    - Now non-verbose by default.
      Enable verbose by setting $VERBOSE
    
    - Foolish attempt at error handling.
    
    - Fallback to cURL if no wget, and yell if that also fails.
    
    - Work on the readability.
    
    - Silenced wget and cURL during install.

--- .sh/minecraft.sh
+++ .sh/minecraft.sh
@@ -4,40 +4,86 @@ if [[ `uname` == "Darwin" ]];  then
 else
     export MCPATH="$HOME/.minecraft"
 fi
+function log {
+    if [[ ! $VERBOSE == "" ]]; then
+        echo $1
+    fi
+}
+
 function minecraft {
-    if [[ "$1" == "install" ]]; then
-        if [ ! -e "$MCPATH/bin" ]; then
-            mkdir -p "$MCPATH/bin"
-            echo "Created path $MCPATH/bin"
-        fi
-        echo "Downloading MinecraftLauncher.jar to $MCPATH/bin..."
-        wget -O "$MCPATH/bin/MinecraftLauncher.jar" "https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft.jar"
-        echo "Minecraft Launcher installed. Have a nice day!"
-    elif [[ "$1" == "" ]]; then
+    # Default to launcher with install if not present.
+    if [[ "$1" == "" ]]; then
+        # Check if the launcher is installed.
         if [ ! -e "$MCPATH/bin/MinecraftLauncher.jar" ]; then
-            minecraft install
+            # Force verbose as we're installing without the user's knowlegde.
+            VERBOSE=1 minecraft install
         fi
+        # Run the Launcher.
         java -Xmx1024M -Xms512M -cp "$MCPATH/bin/MinecraftLauncher.jar" net.minecraft.LauncherFrame
+
+
+    elif [[ "$1" == "install" ]]; then
+        # Create Minecraft/bin if not present
+        if [ ! -e "$MCPATH/bin" ]; then
+            mkdir -p "$MCPATH/bin";
+            log "Created path $MCPATH/bin"
+        fi
+        
+        TargetPath="$MCPATH/bin/MinecraftLauncher.jar"
+        BinaryURL="https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft.jar"
+
+        log "Downloading MinecraftLauncher.jar to $MCPATH/bin..."
+        # Download using wget if present.
+        command -v wget &>/dev/null >&2
+        if [[ $? == "0" ]]; then
+            wget -O $TargetPath $BinaryURL &>/dev/null
+
+        else
+            # Fallback to using cURL.
+            command -v curl &>/dev/null >&2
+            if [[ $? == "0" ]]; then
+                curl -o $TargetPath $BinaryURL &>/dev/null
+
+            # No downloader found, complain.
+            else
+                echo "Neither cURL nor wget was found on your system.\n\nPlease download the Minecraft Launcher from\n\thttp://minecraft.net\nand save it as MinecraftLauncher.jar at\n\t$MCPATH/bin\n"
+            fi
+        fi
+        log "Minecraft Launcher installed. Have a nice day!"
+
+    # Extract the contents of Minecraft.jar to the provided directory.
     elif [[ "$1" == "extract" ]]; then
         if [[ "$2" == "" ]]; then
-            echo "No target path specified."
+            echo "Error: No target path specified."
             exit
         fi
+        # Create the directory if it doesn't exist.
         if [ ! -e "$2" ]; then
-            echo "Creating directory $2"
+            log "Creating directory $2"
             mkdir -p $2
         fi
+        # CD into the target directory.
         prevPWD=`pwd`
         cd $2
-        echo "Extracting into $2"
-        jar xf "$MCPATH/bin/minecraft.jar"
+        log "Extracting into $2"
+        # DO IT.
+        jar xf "$MCPATH/bin/minecraft.jar" || echo "Failed extracting."
+        # CD back to the original directory in case
+        # the user wants to do more stuff there.
         cd $prevPWD
+
+
     elif [[ "$1" == "package" ]]; then
         source="."
         if [[ "$2" != "" ]]; then
             source="$2"
         fi
+        log "Removing Meta-inf files."
         rm $source/META-INF/MOJANG_C.*
-        jar uf "$MCPATH/bin/minecraft.jar" "$source"
+        if [[ $? -gt 0 ]]; then
+            log "Meta-inf files doesn't seem to exist."
+        fi
+        log "Packaging new and changed files into Minecraft.jar"
+        jar uf "$MCPATH/bin/minecraft.jar" "$source/" || echo "Failed packaging."
     fi
 }
