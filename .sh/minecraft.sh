395fefa23085f0b6a4c5a09972c72b1830330cca
Simen Lybekk <simen@lybekk.no>
2013-07-16 22:41:37 +0200

    minecraft.sh: Update the launcher DL URL, it seems to have changed.
    Also, new launcher \o/

--- .sh/minecraft.sh
+++ .sh/minecraft.sh
@@ -14,12 +14,12 @@ function minecraft {
     # Default to launcher with install if not present.
     if [[ "$1" == "" ]]; then
         # Check if the launcher is installed.
-        if [ ! -e "$MCPATH/bin/MinecraftLauncher.jar" ]; then
+        if [ ! -e "$MCPATH/bin/MinecraftLauncher.v2.jar" ]; then
             # Force verbose as we're installing without the user's knowlegde.
             VERBOSE=1 minecraft install
         fi
         # Run the Launcher.
-        java -Xmx1024M -Xms512M -cp "$MCPATH/bin/MinecraftLauncher.jar" net.minecraft.LauncherFrame
+        java -Xmx1024M -Xms512M -jar "$MCPATH/bin/MinecraftLauncher.v2.jar"
 
 
     elif [[ "$1" == "install" ]]; then
@@ -29,10 +29,10 @@ function minecraft {
             log "Created path $MCPATH/bin"
         fi
         
-        TargetPath="$MCPATH/bin/MinecraftLauncher.jar"
-        BinaryURL="https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft.jar"
+        TargetPath="$MCPATH/bin/MinecraftLauncher.v2.jar"
+        BinaryURL="https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar"
 
-        log "Downloading MinecraftLauncher.jar to $MCPATH/bin..."
+        log "Downloading MinecraftLauncher.v2.jar to $MCPATH/bin..."
         # Download using wget if present.
         command -v wget &>/dev/null >&2
         if [[ $? == "0" ]]; then
@@ -46,7 +46,7 @@ function minecraft {
 
             # No downloader found, complain.
             else
-                echo "Neither cURL nor wget was found on your system.\n\nPlease download the Minecraft Launcher from\n\thttp://minecraft.net\nand save it as MinecraftLauncher.jar at\n\t$MCPATH/bin\n"
+                echo "Neither cURL nor wget was found on your system.\n\nPlease download the Minecraft Launcher from\n\thttp://minecraft.net\nand save it as MinecraftLauncher.v2.jar at\n\t$MCPATH/bin\n"
             fi
         fi
         log "Minecraft Launcher installed. Have a nice day!"
