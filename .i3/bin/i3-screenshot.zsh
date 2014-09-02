57f97281b3b7608f23265b66ecb1ec8a88e374fb
Simen Lybekk <simen@lybekk.no>
2014-09-03 01:04:27 +0200

    Use escrotum for region screenshots

--- .i3/bin/i3-screenshot.zsh
+++ .i3/bin/i3-screenshot.zsh
@@ -9,4 +9,8 @@ fi
 if [[ "$2" == "-s" ]]; then
     U="s"
 fi
-scrot -b$U -e "beep -l 10 && ablaze -s$P \$f && beep -f 400 -l 100 -n -f 500 -l 100 && mv \$f $SCREENSHOT_DIR" -q 100
+if [[ $U == 's' ]]; then
+    escrotum -s -e "beep -l 10 && ablaze -s$P \$f && beep -f 400 -l 100 -n -f 500 -l 100 && mv \$f $SCREENSHOT_DIR"
+else
+    scrot -b$U -e "beep -l 10 && ablaze -s$P \$f && beep -f 400 -l 100 -n -f 500 -l 100 && mv \$f $SCREENSHOT_DIR" -q 100
+fi
