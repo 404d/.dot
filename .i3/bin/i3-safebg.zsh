1fa248dca68b37b2c577ae3e6fe4e6741f026417
Simen Lybekk <simen@lybekk.no>
2014-09-06 12:59:22 +0200

    .i3: Fix i3-safebg.zsh's unlock feature

--- .i3/bin/i3-safebg.zsh
+++ .i3/bin/i3-safebg.zsh
@@ -2,7 +2,7 @@ source $HOME/.i3/bin/config.zsh
 
 if [[ "$1" == "1" ]]; then
     touch $GROUNDBREAKER_DIR/run.lock && \
-    rm $GROUNDBREAKER_DIR/bg $GROUNDBREAKER_DIR/lock.png && \
+    rm $GROUNDBREAKER_DIR/bg $GROUNDBREAKER_DIR/stitch.png && \
     cp $GROUNDBREAKER_DIR/black.png $GROUNDBREAKER_DIR/bg && \
     cp $GROUNDBREAKER_DIR/black.png $GROUNDBREAKER_DIR/stitch.png && \
     feh --bg-scale $GROUNDBREAKER_DIR/bg
