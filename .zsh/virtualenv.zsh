ef9f6f28e47099d7900deceefbce68ca452a87af
Simen Lybekk <simen@lybekk.no>
2015-09-28 17:12:17 +0200

    .zsh: Make venv more flexible

--- .zsh/virtualenv.zsh
+++ .zsh/virtualenv.zsh
@@ -2,6 +2,9 @@
 
 # checks for and activates virtualenv
 function venv {
+    if [ -n "${${VIRTUAL_ENV}// /}" ]; then
+        return
+    fi
     if [ -e "bin/activate" ]; then
         source bin/activate
     elif [ -e "venv/bin/activate" ]; then
@@ -12,6 +15,22 @@ function venv {
 # hook to automatically activate virtualenv if present
 function chpwd {
     venv
+}
+
+function deactivate {
+    echo "Not in a virtual environment."
+    return 1
+}
+
+function denv {
+    deactivate
+    r=$?
+
+    function deactivate {
+        echo "Not in a virtual environment."
+        return 1
+    }
+    return $r
 }
 
 # check for virtualenv at launch
